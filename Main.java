6dfdb4323fbe1e177aca4d199c0a165f802a0d12
Glaydson V Sousa <Glaydson@users.noreply.github.com>
2024-05-11 20:13:39 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    
+    Solution1 sl = new Solution1();
+    int [] numbers = {1,2,3,4,5,6,7};
+    System.out.println(sl.firstMissingPositive(numbers));
+   
+    
+  }
+}
